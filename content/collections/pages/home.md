52de573324b45209a862a3e1f0b641fe05e2f4ad
Cornelius van Erkelens <45368779+nouce@users.noreply.github.com>
2023-04-22 10:37:32 +0200

    Update

--- content/collections/pages/home.md
+++ content/collections/pages/home.md
@@ -1,8 +1,22 @@
 ---
-title: Home
 id: home
-template: home
 blueprint: pages
+title: Home
+template: home
+header_title: Kindercoaching
+header_subtitle: 'met een goal'
+section_1_title: 'Laat je kind goal  gericht werken'
+section_1_content:
+  -
+    type: paragraph
+    content:
+      -
+        type: text
+        text: 'Wat fijn dat je een kijkje neemt op de website van GOALZ Kindercoaching! Mijn naam is Gaby van Erkelens en sinds 2022 ben ik gestart met GOALZ Kindercoaching. Na fijne jaren als juf in het basisonderwijs besloot ik de stap te wagen om te beginnen als zelfstandige. Ik sta nog steeds met veel plezier voor de klas, maar mijn interesse ligt ook bij het individueel begeleiden en coachen van kinderen. Vanuit mijn christelijke identiteit vind ik het ook erg belangrijk om anderen te helpen en ik heb hier zelf altijd een passie voor gehad. Ik ben dankbaar dat ik dit nu iedere mag doen met GOALZ Kindercoaching.'
+section_1_image_1: adam-winger-7ff0iei80aq-unsplash.jpg
+section_1_image_2: kenny-eliason-1-aa2fadydc-unsplash.jpg
+updated_by: a474421d-951b-4c31-9d82-acd952f5335c
+updated_at: 1682151985
 ---
 ## Welcome to your brand new Statamic site!
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,58 @@
 module.exports = {
-  content: [
-    './resources/**/*.antlers.html',
-    './resources/**/*.blade.php',
-    './resources/**/*.vue',
-    './content/**/*.md'
-  ],
-  theme: {
-    extend: {},
+  mode: 'jit',
+  purge: {
+    content: [
+      './resources/**/*.antlers.html',
+      './resources/**/*.blade.php',
+      './resources/**/*.vue',
+      './content/**/*.md'
+    ]
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  important: false,
+  theme: {
+    screens: {
+        'xxl': {'max': '1440px'},
+        '<xxl': {'min': '1439px'},
+
+        'xl': {'max': '1280px'},
+        '<xl': {'min': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+
+        'lg': {'max': '1024px'},
+        '<lg': {'min': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+
+        'lg-md': {'max': '890px'},
+        '<lg-md': {'min': '889px'},
+
+        'md': {'max': '768px'},
+        '<md': {'min': '767px'},
+        // => @media (max-width: 767px) { ... }
+
+        'sm': {'max': '640px'},
+        '<sm': {'min': '639px'},
+        // => @media (max-width: 639px) { ... }
+
+        'xs': {'max': '400px'},
+        '<xs': {'min': '400px'},
+        // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+        width: {
+            container: '90%'
+        },
+        inset: {
+            container: '5%'
+        },
+        colors: {
+            primary: '#827855',
+            light: '#DADBD6'
+        },
+        fontFamily: {
+            'sans' : ['Josefin Sans', 'sans-serif']
+        }
+    },
+  },
+  variants: {},
+  plugins: [],
 }
